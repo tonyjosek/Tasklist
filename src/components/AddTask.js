c86e488b78cb5ae329cbcf061b9015092ae5c2ec
Tony <tonykurisummoodan>
2023-10-16 13:03:59 +0200

    Project Tasklist

---
+++ src/components/AddTask.js
@@ -0,0 +1,35 @@
+
+export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if(task.id)
+        {
+            const date= new Date();
+            const updatedTasklist = tasklist.map((todo) =>(
+                todo.id === task.id ? {id: task.id, name:e.target.task.value, time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+            ));
+            setTasklist(updatedTasklist);
+            setTask({});
+        }else{
+            const date =new Date();
+            const newTask ={
+                id:date.getTime(),
+                name:e.target.task.value,
+                time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+            }
+            setTasklist([...tasklist, newTask]);
+            setTask({});
+        }
+
+       
+    }
+  return (
+    <section className="addTask">
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="task" value={task.name || ""} autoComplete="off" maxLength="25" placeholder="Add Task" onChange={e => setTask({...task, name: e.target.value})} />
+            <button type="sunmit">{task.id ? "Update":"Add"}</button>
+        </form>
+    </section>
+  )
+}
